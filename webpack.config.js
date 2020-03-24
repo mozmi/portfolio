540d77d2a57d750c5715d3c5544719330de18b74
mozmi <mozmi.430@gmail.com>
2020-03-24 22:55:23 +0900

    first commit.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+// 'production' or 'development'
+const MODE = 'development';
+
+const enabledSourceMap = (MODE === 'development');
+
+module.exports = {
+  mode: MODE,
+
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: enabledSourceMap,
+              importLoaders: 2
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: enabledSourceMap,
+            }
+          }
+        ],
+      },
+    ],
+  },
+};
